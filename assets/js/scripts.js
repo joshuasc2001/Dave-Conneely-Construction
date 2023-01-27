18e125161d3541ba9efddf03dfc0ef2f0174ea7f
joshuasc2001 <50122884+joshuasc2001@users.noreply.github.com>
2023-01-27 20:14:24 +0000

    Redesign

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,60 @@
-$(document).ready(function() {
+const navToggle = document.querySelector('.nav-toggle');
+const mainNav = document.querySelector('.main-nav');
+const hamburger = document.querySelector('.hamburger');
+const pathname = window.location.pathname;
+let width = document.body.clientWidth;
 
-  // hamburger menu toggle
-  $('.nav-toggle').click(function() {
-    $('.main-nav').toggleClass('is-open');
-    $('.hamburger').toggleClass('is-open');
-  })
+const imgHTML = `
+<div class="header-logo" id='nav-logo'>
+  <a href="${pathname === '/index.html' ? './index.html' : '../index.html'}">
+      <img src="${pathname === '/index.html' ? './assets/images/logo.webp' : '../assets/images/logo.webp'}" alt="Dave conneely construction Logo" />
+  </a>
+</div> 
+${mainNav.innerHTML}`;
+
+window.addEventListener('resize', event => {
+  const navLogo = document.getElementById('nav-logo');
+  if (navLogo) {
+    navLogo.remove();
+  }
+  width = document.body.clientWidth;
+  sponsorSlidshow()
+
+});
+
+navToggle.addEventListener('click', (event) => {
+  handleNav();
+});
+
+navToggle.addEventListener('keydown', (event) => {
+
+  if (event.key === 'Enter' || event.key === ' ') {
+    event.preventDefault();
+    handleNav();
+  }
 
 })
+
+const handleNav = () => {
+  mainNav.classList.toggle('is-open');
+  hamburger.classList.toggle('is-open');
+
+  if (!document.getElementById('nav-logo')) {
+    mainNav.innerHTML = imgHTML;
+  } else {
+    const navLogo = document.getElementById('nav-logo');
+    navLogo.remove();
+  }
+
+  if (!hamburger.classList.contains('is-open') && width < 1000) {
+    const navLinks = document.querySelectorAll('nav-links');
+    navLinks.forEach(element => {
+      element.tabIndex = -1;
+    });
+  } else {
+    const navLinks = document.querySelectorAll('nav-links');
+    navLinks.forEach(element => {
+      element.tabIndex = 0;
+    });
+  }
+}
